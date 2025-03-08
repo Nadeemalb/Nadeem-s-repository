aae7cc7c676d689cff21cdeaa85db483d34e855a
Nadeemalb <barwani@usf.edu>
2025-03-08 17:26:59 -0500

    Update Module # 8 Correlation Analysis and ggplot2.R

--- Module # 8 Correlation Analysis and ggplot2.R
+++ Module # 8 Correlation Analysis and ggplot2.R
@@ -49,4 +49,5 @@ grid.arrange(plot1, plot2, plot3, ncol=2)
 # Save the plot as a PNG file
 ggsave("scatter_plots_grid.png", plot = grid.arrange(plot1, plot2, plot3, ncol=2))
 
-
+"Few's recommendation to use grid layouts for visual comparisons is incredibly useful, as it helps to avoid clutter and makes it easier for the viewer to see the relationships between multiple variables at once. By arranging multiple scatter plots in a grid format, we can make better-informed comparisons and detect patterns, correlations, or outliers more clearly. It’s especially helpful when dealing with multivariate data, such as in the mtcars dataset, where multiple variables influence the performance of cars. The use of regression lines in combination with scatter plots further enhances the understanding of the relationship between these variables."
+file:///C:/Users/user/OneDrive/Documents/Module---8-Correlation-Analysis-and-ggplot2.html  
